ef2dcf426f56f9ffea5c5e8d2e6021024cd2f5fe
zeldal <zeldalozdemir@gmail.com>
2017-02-18 16:31:48 +0300

    - Operation Store Implementation

--- sample-client/src/main/resources/create-eventdb.sql
+++ sample-client/src/main/resources/create-eventdb.sql
@@ -1,7 +1,10 @@
-CREATE TABLE rank_by_year_and_name (
-race_year int,
-race_name text,
-cyclist_name text,
-rank int,
-PRIMARY KEY ((race_year, race_name), rank)
+drop table AggregateEvent;
+CREATE TABLE AggregateEvent (
+	entityId bigint,
+	aggregateName ascii,
+	opId UUID,
+	opDate timestamp,
+	status varchar,
+	description varchar,
+	PRIMARY KEY (entityId, opDate, opId, aggregateName)
 );
