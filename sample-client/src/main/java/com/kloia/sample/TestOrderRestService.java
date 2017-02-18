50106b93f8bebc421994a5c6fecb7b6652012909
Mesut Can Gurle <mesutcang@gmail.com>
2017-02-18 18:31:05 +0300

    feign client support is added.

--- sample-client/src/main/java/com/kloia/sample/TestOrderRestService.java
+++ sample-client/src/main/java/com/kloia/sample/TestOrderRestService.java
@@ -11,18 +11,28 @@ import com.kloia.evented.AggregateRepository;
 import com.kloia.sample.dto.Order;
 import com.kloia.sample.dto.OrderCreateAggDTO;
 import com.kloia.sample.dto.OrderProcessAggDTO;
+import feign.Feign;
+import feign.RequestLine;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.feign.support.ResponseEntityDecoder;
+import org.springframework.cloud.netflix.feign.support.SpringDecoder;
+import org.springframework.cloud.netflix.feign.support.SpringEncoder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.io.IOException;
@@ -30,6 +40,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  * Created by zeldalozdemir on 09/02/2017.
@@ -38,6 +51,7 @@ import java.util.UUID;
 @SpringBootApplication
 @RequestMapping(value = "/aggr/v1/order/")
 @SpringBootConfiguration()
+@EnableFeignClients
 public class TestOrderRestService {
 
     @Autowired
@@ -63,8 +77,10 @@ public class TestOrderRestService {
         Map.Entry<UUID, Operation> context = operationRepository.getContext();
         String description = objectMapper.writer().writeValueAsString(orderCreateAggDTO);
         AggregateEvent aggregateEvent = new AggregateEvent(new AggregateKey(orderCreateAggDTO.getOrderId(),new Date(), context.getKey(),"CREATE_ORDER"), "CREATED", description);
-        AggregateEvent eventRecorded = aggregateRepository.recordAggregate(aggregateEvent);
-        return new ResponseEntity<Object>(eventRecorded, HttpStatus.CREATED);
+//        AggregateEvent eventRecorded = aggregateRepository.recordAggregate(aggregateEvent);
+        PaymentEndpoint paymentEndpoint = Feign.builder().decoder(feignDecoder()).encoder(feignEncoder()).target(PaymentEndpoint.class, "http://localhost:8080");
+        OrderCreateAggDTO orderCreateAggDTO1 = paymentEndpoint.create(orderCreateAggDTO);
+        return new ResponseEntity<Object>(orderCreateAggDTO1, HttpStatus.CREATED);
     }
 
 
@@ -85,5 +101,22 @@ public class TestOrderRestService {
                 .properties(props)
                 .run(args);
     }
+    public Decoder feignDecoder() {
+        HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(new ObjectMapper());
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
+        return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
+    }
 
+    public Encoder feignEncoder() {
+        HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(new ObjectMapper());
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
+        return new SpringEncoder(objectFactory);
+    }
+
+}
+
+interface PaymentEndpoint{
+    //@RequestMapping(value = "/aggr/v1/payment/create", method = RequestMethod.POST)
+    @RequestLine("GET /aggr/v1/payment/create")
+    OrderCreateAggDTO create(@RequestBody OrderCreateAggDTO orderCreateAggDTO);
 }
