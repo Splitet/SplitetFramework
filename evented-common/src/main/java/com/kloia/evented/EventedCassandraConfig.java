fd6bf1e16b9b211a4b7e8acac925c24d95c6fe98
zeldal <zeldalozdemir@gmail.com>
2017-05-05 17:56:24 +0300

    - Event api cassandra naming fix

--- evented-common/src/main/java/com/kloia/evented/EventedCassandraConfig.java
+++ evented-common/src/main/java/com/kloia/evented/EventedCassandraConfig.java
@@ -1,40 +1,53 @@
 package com.kloia.evented;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
 import org.springframework.data.cassandra.config.CassandraSessionFactoryBean;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.convert.CassandraConverter;
 import org.springframework.data.cassandra.convert.MappingCassandraConverter;
-import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.data.cassandra.core.CassandraTemplate;
 import org.springframework.data.cassandra.mapping.BasicCassandraMappingContext;
 import org.springframework.data.cassandra.mapping.CassandraMappingContext;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
 
 /**
  * Created by zeldalozdemir on 12/02/2017.
  */
 @Slf4j
 @Configuration
-@PropertySource(value = { "classpath:event-store.properties" })
 @EnableCassandraRepositories(basePackages = { "com.kloia.evented" })
 public class EventedCassandraConfig {
-    @Autowired
-    private Environment env;
+    @Value("${cassandra.contact-points:'localhost:9042}")
+    private String contactPoints;
+
+    @Value("${cassandra.keyspace-name:'test'}")
+    private String keyspaceName;
 
     @Bean
     public CassandraClusterFactoryBean cluster() {
 
         CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
-        cluster.setContactPoints(env.getProperty("cassandra.contact-points"));
-        cluster.setPort(Integer.parseInt(env.getProperty("cassandra.port")));
+        ArrayList<InetSocketAddress> addresses = new ArrayList<>();
+        try {
+            String[] hostPorts = contactPoints.split(";");
+            for (String hostPort : hostPorts) {
+                String[] split = hostPort.split(":");
+                addresses.add(new InetSocketAddress(split[0], Integer.parseInt(split[1])));
+            }
+        } catch (Exception e) {
+            log.error("cassandra.contact-points must be: host1:port1[;host2:port2]... :",e);
+            throw e;
+        }
 
+        cluster.setClusterBuilderConfigurer(clusterBuilder -> clusterBuilder.addContactPointsWithPorts(addresses));
+//        cluster.setContactPoints("cassandra1");
         return cluster;
     }
 
@@ -53,7 +66,7 @@ public class EventedCassandraConfig {
 
         CassandraSessionFactoryBean session = new CassandraSessionFactoryBean();
         session.setCluster(cluster().getObject());
-        session.setKeyspaceName(env.getProperty("cassandra.keyspace-name"));
+        session.setKeyspaceName(keyspaceName);
         session.setConverter(converter());
         session.setSchemaAction(SchemaAction.NONE);
 
