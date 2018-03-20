d32819ec03b88bb0bc98b2e7d50d7721d7459800
zeldal <zeldalozdemir@gmail.com>
2018-03-20 16:06:51 +0300

    eventapis(#22) Initial Release for New Features

--- samples/3-advanced/order-service/src/main/java/com/kloia/sample/dto/event/OrderCreatedEvent.java
+++ samples/3-advanced/order-service/src/main/java/com/kloia/sample/dto/event/OrderCreatedEvent.java
@@ -2,6 +2,7 @@ package com.kloia.sample.dto.event;
 
 import com.kloia.eventapis.common.EventType;
 import com.kloia.eventapis.common.PublishedEvent;
+import com.kloia.eventapis.common.ReceivedEvent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,13 +10,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class OrderCreatedEvent extends PublishedEvent {
+public class OrderCreatedEvent extends ReceivedEvent {
     private String stockId;
     private int orderAmount;
     private String description;
 
-    @Override
-    public EventType getEventType() {
-        return EventType.OP_SINGLE;
-    }
+//    @Override
+//    public EventType getEventType() {
+//        return EventType.OP_SINGLE;
+//    }
 }
